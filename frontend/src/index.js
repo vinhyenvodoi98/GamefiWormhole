cd1ff06a73b21f30297aa526b56d6fcded27d821
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2023-04-15 21:53:49 +0900

    feat: add Linaio gate

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './routers/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
--- frontend/src/routers/Home/index.js
+++ frontend/src/routers/Home/index.js
@@ -5,18 +5,16 @@ const Home = () => {
         <Row justify="center" align="middle" style={{ height: '100vh' }}>
             <Col xs={20} sm={16} md={12} lg={8} xl={6}>
                 <div style={{ textAlign: 'center' }}>
-                <h1>Trang Input</h1>
-                <p>Nhập thông tin của bạn:</p>
+                <h1>Linaio Gate</h1>
+                <p>Crosschain collateral</p>
                 </div>
-                <Input placeholder="Họ và tên" style={{ marginBottom: 16 }} />
-                <Input placeholder="Địa chỉ email" style={{ marginBottom: 16 }} />
-                <Input.Password placeholder="Mật khẩu" style={{ marginBottom: 16 }} />
+                <Input placeholder="0xaaaa" style={{ marginBottom: 16 }} />
                 <Button type="primary" block>
-                Đăng ký
+                    Submit
                 </Button>
             </Col>
         </Row>
-      );
+    );
 }
 
 export default Home;
